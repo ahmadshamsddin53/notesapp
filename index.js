67aec5597c9b485c9dc145943ddc44260c3bc56a
unknown <ahmadshamsddin53@gmail.com>
2020-11-27 14:00:28 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(expressLayouts)
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/note', (req, res) => {
+    res.render('note')
+})
+
+app.listen(8000, '0.0.0.0')
